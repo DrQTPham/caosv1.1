b1587bd0e23bdd6e212d3ec975e6477ed0f995c2
DrQTPham <qtphamhus@gmail.com>
2025-08-08 13:13:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,300 +1,168 @@
 function calculate() {
-    var inputNumber = document.getElementById("inputNumber").value;
+    const inputNumber = document.getElementById("inputNumber").value;
+    const number = parseFloat(inputNumber);
 
-    var number = parseFloat(inputNumber);
-    var d = Number.isInteger(number) && number > 0;
-    if (isNaN(number) || !d) {
-        document.getElementById("result").style.color = "Red";
-        if (isNaN(number))
-            document.getElementById("result").textContent = "The input is not a number !!";
-        else
-            document.getElementById("result").textContent = "You must enter a natural number !!";
-    } else {
-        document.getElementById("result").textContent = addInputFields(number);
+    if (isNaN(number) || !Number.isInteger(number) || number <= 0) {
+        const result = document.getElementById("result");
+        result.style.color = "red";
+        result.textContent = isNaN(number)
+            ? "The input is not a number !!"
+            : "You must enter a natural number !!";
+        return;
     }
+
+    document.getElementById("result").textContent = "";
+    addInputFields(number);
 }
+
 function addInputFields(count) {
-    var container = document.querySelector(".container");
+    const container = document.querySelector(".container");
     container.innerHTML = "";
-    var h2 = document.createElement("h2");
-    h2.innerHTML = "Please enter details for the target volumes: ";
-    h2.innerHTML += "<br> (Volume: V; Coordinate: x,y,z)";
+
+    const h2 = document.createElement("h2");
+    h2.innerHTML = "Please enter details for the target volumes: <br>(Volume: V; Coordinate: x, y, z)";
     container.appendChild(h2);
-    function truc_x(c) {
-        var radians = c * Math.PI / 180;
-        return [
-            [Math.cos(radians), 0, Math.sin(radians)],
-            [0, 1, 0],
-            [-Math.sin(radians), 0, Math.cos(radians)]
-        ];
-    }
 
-    function truc_y(t) {
-        var radians = t * Math.PI / 180;
-        return [
-            [Math.cos(radians), 0, Math.sin(radians)],
-            [0, 1, 0],
-            [-Math.sin(radians), 0, Math.cos(radians)]
-        ];
-    }
-
-    function truc_z(g) {
-        var radians = g * Math.PI / 180;
-        return [
-            [Math.cos(radians), -Math.sin(radians), 0],
-            [Math.sin(radians), Math.cos(radians), 0],
-            [0, 0, 1]
-        ];
-    }
-    for (var i = 0; i < count; i++) {
-        var inputContainer = document.createElement("div");
+    for (let i = 0; i < count; i++) {
+        const inputContainer = document.createElement("div");
         inputContainer.classList.add("input-container");
 
-        var inputVolume = document.createElement("input");
-        inputVolume.type = "number";
-        inputVolume.placeholder = "V_PTV " + (i + 1);
-        inputContainer.appendChild(inputVolume);
-
-        var inputAxis = document.createElement("input");
-        inputAxis.type = "number";
-        inputAxis.placeholder = "x_PTV " + (i + 1);
-        inputContainer.appendChild(inputAxis);
-
-        inputAxis = document.createElement("input");
-        inputAxis.type = "number";
-        inputAxis.placeholder = "y_PTV " + (i + 1);
-        inputContainer.appendChild(inputAxis);
-
-        inputAxis = document.createElement("input");
-        inputAxis.type = "number";
-        inputAxis.placeholder = "z_PTV " + (i + 1);
-        inputContainer.appendChild(inputAxis);
+        ["V", "x", "y", "z"].forEach(coord => {
+            const input = document.createElement("input");
+            input.type = "number";
+            input.placeholder = `${coord}_PTV ${i + 1}`;
+            inputContainer.appendChild(input);
+        });
 
         container.appendChild(inputContainer);
     }
 
-    var button = document.createElement("button");
-    button.textContent = "Planning Treatment Volume";
-    var max1 = -999999;
-    var min1 = 99999;
-    var max2 = -999999;
-    var min2 = 99999;
-    var max3 = -999999;
-    var min3 = 99999;
-    button.onclick = function () {
-        var inputContainers = document.querySelectorAll(".input-container");
-        inputContainers.forEach(function (container) {
-            var inputVolumes = container.querySelectorAll("input[type='number']");
-            var inputVolume = inputVolumes[0];
-            var V = parseFloat(inputVolume.value);
-            var r = ((3 * V) / (4 * Math.PI)) ** (1 / 3);
-            var r2, r3, r4;
-            if (inputVolumes.length >= 3) {
-                var inputVolume2 = inputVolumes[1];
-                r2 = parseFloat(inputVolume2.value);
-                if (max1 < r2) {
-                    max1 = r2;
-                };
-                if (min1 > r2) {
-                    min1 = r2;
-                };
-                var inputVolume3 = inputVolumes[2];
-                r3 = parseFloat(inputVolume3.value);
-                if (max2 < r3) {
-                    max2 = r3;
-                };
-                if (min2 > r3) {
-                    min2 = r3;
-                };
-                var inputVolume4 = inputVolumes[3];
-                r4 = parseFloat(inputVolume4.value);
-                if (max3 < r4) {
-                    max3 = r4;
-                };
-                if (min3 > r4) {
-                    min3 = r4;
-                };
-            }
-            var resultDisplay = document.createElement("div");
-            resultDisplay.textContent = "Rayon = " + r.toFixed(2) + " (cm)";
-            if (inputVolumes.length >= 3) {
-                resultDisplay.textContent += ",  x = " + r2.toFixed(2) + " (cm),  y = " + r3.toFixed(2) + " (cm),  z = " + r4.toFixed(2) + " (cm)";
-            }
-            container.appendChild(resultDisplay);
-        });
-        var xiso = 0;
-        var yiso = 0;
-        var ziso = 0;
-        var Display1 = document.createElement("div");
-        Display1.classList.add("classIsocenter");
-        Display1.textContent = "ISOCENTER coordinate : ";
-        container.appendChild(Display1);
-        var Display = document.createElement("div");
-        Display.classList.add("classIsocenter2");
-        if ((max1 == min1) != 0) {
-            xiso = max1;
-        };
-        if (max1 > 0 && min1 > 0) {
-            xiso = (max1 - min1) / 2 + min1;
-        };
-        if (max1 < 0 && min1 < 0) {
-            xiso = (min1 - max1) / 2 + max1;
-        };
-        if (min1 <= 0 && max1 >= 0) {
-            xiso = (max1 + min1) / 2;
-        };
+    const planButton = document.createElement("button");
+    planButton.textContent = "Planning Treatment Volume";
+    planButton.onclick = computeIsocenterAndVolume;
+    container.appendChild(planButton);
+}
 
-        if ((max2 == min2) != 0) {
-            yiso = max2;
-        };
-        if (max2 > 0 && min2 > 0) {
-            yiso = (max2 - min2) / 2 + min2;
-        };
-        if (max2 < 0 && min2 < 0) {
-            yiso = (min2 - max2) / 2 + max2;
-        };
-        if (min2 <= 0 && max2 >= 0) {
-            yiso = (max2 + min2) / 2;
-        };
+function computeIsocenterAndVolume() {
+    const container = document.querySelector(".container");
+    const inputContainers = document.querySelectorAll(".input-container");
 
-        if ((max3 == min3) != 0) {
-            ziso = max3;
-        };
-        if (max3 > 0 && min3 > 0) {
-            ziso = (max3 - min3) / 2 + min3;
-        };
-        if (max3 < 0 && min3 < 0) {
-            ziso = (min3 - max3) / 2 + max3;
-        };
-        if (min3 <= 0 && max3 >= 0) {
-            ziso = (max3 + min3) / 2;
-        };
-        Display.innerHTML = " x (Isocenter) is: " + xiso.toFixed(2) + " (cm)" + "<br> y  (Isocenter) is: " + yiso.toFixed(2) + " (cm)" + "<br> z(Isocenter) is: " + ziso.toFixed(2) + " (cm)";
-        container.appendChild(Display);
-        var button2 = document.createElement("button");
-        button2.textContent = "Optimization of SRS";
-        button2.classList.add("twoToneButton");
-        button2.onclick = function () {
-            //Optima Collimator
-        
-            function table(t) {
-                var a = -99999999999999;
-                var n1 = 0;
-                for (var c = 0; c < 181; c++) {
-                    var s = 0;
-                    var R4 = 0;
-                    var R5 = 0;
-                    var R6 = 0;
-                    for (var g = 0; g < 361; g++) {
-                        var b2 = [];
-                        inputContainers.forEach(function (container2) {
-                            var inputVolumes2 = container2.querySelectorAll("input[type='number']");
-                            var trucxPTV = inputVolumes2[1];
-                            var trucxPTV1 = parseFloat(trucxPTV.value);
-                            var trucyPTV = inputVolumes2[2];
-                            var trucyPTV1 = parseFloat(trucyPTV.value);
-                            var truczPTV = inputVolumes2[3];
-                            var truczPTV1 = parseFloat(truczPTV.value);
-                            var toadoV = [trucxPTV1, trucyPTV1, truczPTV1];
-                            var R1 = math.multiply(truc_z(g), truc_x(c));
-                            var R2 = math.multiply(R1, truc_y(t));
-                            var R3 = math.multiply(R2, toadoV);
-                            var R33 = new Array(R3);
+    let xValues = [], yValues = [], zValues = [];
 
-                            var Volume = inputVolumes2[0];
-                            var V2 = parseFloat(Volume.value);
-                            var bankinhr = ((3 * V2) / (4 * Math.PI)) ** (1 / 3);
+    inputContainers.forEach(container => {
+        const inputs = container.querySelectorAll("input[type='number']");
+        const V = parseFloat(inputs[0].value);
+        const x = parseFloat(inputs[1].value);
+        const y = parseFloat(inputs[2].value);
+        const z = parseFloat(inputs[3].value);
 
-                            R4 = R33[0][0] - bankinhr;
-                            R5 = R33[0][2] + bankinhr;
-                            R6 = R33[0][2] - bankinhr;
-                            var R4n = Math.round(R4);
-                            var R5n = Math.round(R5);
-                            var R6n = Math.round(R6); 
-                            for (var k = R6n; k <= R5n; k++) {
-                                var temp2 = [];
-                                for (var m = 0; m < 2; m++) {
-                                    var h = (m === 0) ? R4n : k;
-                                    temp2.push(h);
-                                }
-                                b2.push(temp2);
-                            }
-                        });
-                       
-                        var n2 = b2.length;
-                        var b3 = [];
+        if (!isNaN(x)) xValues.push(x);
+        if (!isNaN(y)) yValues.push(y);
+        if (!isNaN(z)) zValues.push(z);
 
-                        for (var o = -15; o < 15; o++) {
-                            for (var q = -15; q < 15; q++) {
-                                b3.push([q, o]);
-                                var shouldBreak = false;
-                                for (var v = 0; v < n2; v++) {
-                                    if (o === b2[v][1] && q >= b2[v][0]) {
-                                        shouldBreak = true;
-                                        break;
-                                    }
-                                }
-                                if (shouldBreak) {
-                                    break;
-                                }
-                            }
-                        }
-                      
-                        var sodiem = b3;
-                        var so = sodiem.length;
-                        s += so;
-                    }
-                    if (s >= a) {
-                        a = s;
-                        n1 = c;
-                    } 
-                } 
-                return n1;
+        const radius = ((3 * V) / (4 * Math.PI)) ** (1 / 3);
+        const result = document.createElement("div");
+        result.textContent = `Radius = ${radius.toFixed(2)} cm, x = ${x} cm, y = ${y} cm, z = ${z} cm`;
+        container.appendChild(result);
+    });
+
+    const xiso = (Math.max(...xValues) + Math.min(...xValues)) / 2;
+    const yiso = (Math.max(...yValues) + Math.min(...yValues)) / 2;
+    const ziso = (Math.max(...zValues) + Math.min(...zValues)) / 2;
+
+    const isoDisplay = document.createElement("div");
+    isoDisplay.classList.add("isocenter-display");
+    isoDisplay.innerHTML = `ISOCENTER Coordinates:<br>x = ${xiso.toFixed(2)} cm<br>y = ${yiso.toFixed(2)} cm<br>z = ${ziso.toFixed(2)} cm`;
+    container.appendChild(isoDisplay);
+
+    const optimizeButton = document.createElement("button");
+    optimizeButton.textContent = "Optimize Collimator Angle";
+    optimizeButton.onclick = () => optimizeCollimator(inputContainers);
+    container.appendChild(optimizeButton);
+}
+
+function optimizeCollimator(inputContainers) {
+    const container = document.querySelector(".container");
+
+    function rotationMatrix(axis, angle) {
+        const rad = angle * Math.PI / 180;
+        if (axis === 'x') {
+            return [
+                [1, 0, 0],
+                [0, Math.cos(rad), -Math.sin(rad)],
+                [0, Math.sin(rad), Math.cos(rad)]
+            ];
+        } else if (axis === 'y') {
+            return [
+                [Math.cos(rad), 0, Math.sin(rad)],
+                [0, 1, 0],
+                [-Math.sin(rad), 0, Math.cos(rad)]
+            ];
+        } else if (axis === 'z') {
+            return [
+                [Math.cos(rad), -Math.sin(rad), 0],
+                [Math.sin(rad), Math.cos(rad), 0],
+                [0, 0, 1]
+            ];
+        }
+    }
+
+    function computeAngle(t) {
+        let maxScore = -Infinity;
+        let bestAngle = 0;
+
+        for (let c = 0; c <= 180; c++) {
+            let score = 0;
+
+            for (let g = 0; g <= 360; g++) {
+                inputContainers.forEach(container => {
+                    const inputs = container.querySelectorAll("input[type='number']");
+                    const V = parseFloat(inputs[0].value);
+                    const x = parseFloat(inputs[1].value);
+                    const y = parseFloat(inputs[2].value);
+                    const z = parseFloat(inputs[3].value);
+
+                    const radius = ((3 * V) / (4 * Math.PI)) ** (1 / 3);
+                    const coord = [x, y, z];
+
+                    let R = math.multiply(
+                        rotationMatrix('z', g),
+                        rotationMatrix('x', c)
+                    );
+                    R = math.multiply(R, rotationMatrix('y', t));
+                    const rotated = math.multiply(R, coord);
+
+                    if (rotated[0] - radius < 0 && rotated[2] + radius > 0) score++;
+                });
             }
 
-            var Display2 = document.createElement("div");
-            Display2.classList.add("collimator");
-            t = 0;
-            Display2.innerHTML = 'Couch angle: C = 0° , Gantry angle: 0 CW 179,  Optimized collimator angle: ' + table(0) + '°';
-            t = 335;
-            Display2.innerHTML += '<br> Couch angle: C = 335° , Gantry angle: 179 CCW 0, Optimized collimator angle: ' + table(335) + '°';
-            t = 310;
-            Display2.innerHTML += '<br>Couch angle: C = 310° , Gantry angle: 0 CW 179, Optimized collimator angle: ' + table(310) + '°';
-            t = 285;
-            Display2.innerHTML += '<br>Couch angle: C = 285° , Gantry angle: 179 CCW 0, Optimized collimator angle: ' + table(285) + '°';
-            t = 0;
-            Display2.innerHTML += '<br>Couch angle: C = 0° , Gantry angle: 0 CCW 181, Optimized collimator angle: ' + table(0) + '°';
-            t = 25;
-            Display2.innerHTML += '<br>Couch angle: C = 25° , Gantry angle: 181 CW 0, Optimized collimator angle: ' + table(25) + '°';
-            t = 50;
-            Display2.innerHTML += '<br>Couch angle: C = 50° , Gantry angle: 0 CCW 181, Optimized collimator angle: ' + table(50) + '°';
-            t = 75;
-            Display2.innerHTML += '<br>Couch angle: C = 75° , Gantry angle: 181 CW 0, Optimized collimator angle: ' + table(75) + '°';
-            container.appendChild(Display2);
-        };
-        container.appendChild(button2);
-    };
-    container.appendChild(button);
-};
+            if (score > maxScore) {
+                maxScore = score;
+                bestAngle = c;
+            }
+        }
+        return bestAngle;
+    }
 
+    const anglesToTest = [0, 335, 310, 285, 25, 50, 75];
+    const output = document.createElement("div");
+    output.classList.add("collimator-results");
+    output.innerHTML = "<h3>Optimized Collimator Angles:</h3>";
+    anglesToTest.forEach(t => {
+        const angle = computeAngle(t);
+        output.innerHTML += `Couch: ${t}°, Optimal Collimator Angle: ${angle}°<br>`;
+    });
+    container.appendChild(output);
+}
 
+// UI protection
 
-document.addEventListener("DOMContentLoaded", function () {
-    var body = document.body;
-
-    body.style.backgroundColor = "lightblue";
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.backgroundColor = "lightblue";
 });
 
-document.addEventListener("keydown", function(event) {
-    if (event.ctrlKey) {
-        event.preventDefault();
-    }
-    if (event.keyCode == 123) {
-        event.preventDefault();
-    }
+document.addEventListener("keydown", event => {
+    if (event.ctrlKey || event.keyCode === 123) event.preventDefault();
 });
-document.addEventListener('contextmenu', event => event.preventDefault());
 
-
-  
-
+document.addEventListener("contextmenu", event => event.preventDefault());
